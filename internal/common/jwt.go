1a44a5f87d7a52e9b98e3e0160364e62341fde81
Nzyazin <zyazin.nikita@yandex.ru>
2025-01-25 13:00:12 +0300

    added common

---
+++ internal/common/jwt.go
@@ -0,0 +1,39 @@
+package common
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt/v4"
+	"time"
+)
+
+var JWTSecret = []byte("supersecretkey252")
+
+func GenerateToken(userID, role string) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"role":    role,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(JWTSecret)
+}
+
+func ParseToken(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
+		return JWTSecret, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, errors.New("invalid token")
+}
---
+++ internal/common/logger.go
@@ -0,0 +1,24 @@
+package common
+
+import "log"
+
+type Logger interface {
+	Infof(format string, args ...interface{})
+	Errorf(format string, args ...interface{})
+	Warnf(format string, args ...interface{})
+}
+
+type SimpleLogger struct {
+}
+
+func (s *SimpleLogger) Infof(format string, args ...interface{}) {
+	log.Printf("[INFO] "+format, args...)
+}
+
+func (l *SimpleLogger) Warnf(format string, args ...interface{}) {
+	log.Printf("[WARN] "+format, args...)
+}
+
+func (l *SimpleLogger) Errorf(format string, args ...interface{}) {
+	log.Printf("[ERROR] "+format, args...)
+}
