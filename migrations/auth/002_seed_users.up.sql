cfdf5daf0909a6696ca2e9d1fc7e225c37f52453
Nzyazin <zyazin.nikita@yandex.ru>
2025-02-03 21:50:26 +0300

    updated sql

--- migrations/auth/002_seed_users.up.sql
+++ migrations/auth/002_seed_users.up.sql
@@ -1,6 +1,6 @@
 -- Insert admin user with bcrypt hashed password
 -- Default password: admin123 (you should change this in production)
-INSERT INTO users (username, password, role)
+INSERT INTO users (username, password)
 VALUES 
-    ('admin', '$2a$10$aqtifq5lIBLZT8AckY.B0OU7PNW6xAgNIsZW21PU.53HFH1aci2AS', 'admin')
+    ('admin', '$2a$10$aqtifq5lIBLZT8AckY.B0OU7PNW6xAgNIsZW21PU.53HFH1aci2AS')
 ON CONFLICT (username) DO NOTHING;
