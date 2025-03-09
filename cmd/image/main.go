b0ad7ff889bc76a058324b266b00c7b4c2e752df
Nzyazin <zyazin.nikita@yandex.ru>
2025-03-09 12:47:57 +0300

    upd

--- cmd/image/main.go
+++ cmd/image/main.go
@@ -1,1 +1,64 @@
 package main
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"context"
+
+	"github.com/Nzyazin/zadnik.store/internal/broker"
+	"github.com/Nzyazin/zadnik.store/internal/common"
+	"github.com/Nzyazin/zadnik.store/internal/image/storage"
+	"github.com/Nzyazin/zadnik.store/internal/image/usecase"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(filepath.Join("internal", "image", "config", ".env-image"))
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	logger := common.NewSimpleLogger()
+
+	messageBroker, err := broker.NewRabbitMQBroker(
+		broker.RabbitMQConfig{
+			URL: os.Getenv("RABBITMQ_URL"),
+		},
+	)
+	if err != nil {
+		log.Fatalf("Failed to initialize message broker: %v", err)
+	}
+	defer messageBroker.Close()
+
+	imageStorage, err := storage.NewFileStorage(
+		os.Getenv("STORAGE_PATH"),
+		os.Getenv("IMAGE_BASE_URL"),
+	)
+	if err != nil {
+		log.Fatalf("Failed to initialize file storage: %v", err)
+	}
+
+	imageUseCase := usecase.NewImageUseCase(imageStorage, messageBroker, logger)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err = messageBroker.SubscribeToImageUpload(ctx, func(event *broker.ImageEvent) error {
+		logger.Infof("Received image upload event for product %d", event.ProductID)
+
+		if err := imageUseCase.ProcessImage(ctx, event.ImageData, event.ProductID); err != nil {
+			logger.Errorf("Failed to process image %v", err)
+			return err
+		}
+
+		logger.Infof("Successfully proccessed image for product %d", event.ProductID)
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("Failed to subscribe to image upload: %v", err)
+	}
+
+	logger.Infof("Image service started")
+	select {}	
+}
