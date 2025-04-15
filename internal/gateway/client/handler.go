15ba2c92f1c3748aab17e48cfbb7f7f399fb98c7
Nzyazin <nzyazin@gmail.com>
2025-04-15 17:13:37 +0300

    Add client handler and templates structure for product service integration

--- internal/gateway/client/handler.go
+++ internal/gateway/client/handler.go
@@ -1,1 +1,22 @@
 package client
+
+import "github.com/Nzyazin/zadnik.store/internal/templates/client_templates"
+
+type Handler struct {
+	templates *client_templates.Templates
+	productService string
+	apiKey string
+}
+
+func NewHandler(templates *client_templates.Templates, productService string, apiKey string) *Handler {
+	return &Handler{r
+		templates: templates,
+		productService: productService,
+		apiKey: apiKey,
+	}
+}
+		templates: templates,
+		productService: productService,
+		apiKey: apiKey,
+	}
+}
--- internal/templates/client-templates/templates.go
+++ internal/templates/client-templates/templates.go
@@ -1,1 +1,1 @@
-package client
+package client_templates
